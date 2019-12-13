e3d2d50733f3b196f5050fba54fa2a562a268578
Tina Wu <tinawu.nd23@gmail.com>
2019-12-13 16:35:12 -0500

    fix frontend file boxes

--- frontend/animate.js
+++ frontend/animate.js
@@ -1,13 +1,50 @@
 $(document).ready(function () {
-    $('#chooseFile').bind('change', function () {
-        var filename = $("#chooseFile").val();
+    $('#chooseFile1').bind('change', function () {
+        var filename = $("#chooseFile1").val();
         if (/^\s*$/.test(filename)) {
-            $(".file-upload").removeClass('active');
-            $("#noFile").text("No file chosen...");
+            $(".file-upload1").removeClass('active');
+            $("#noFile1").text("No file chosen...");
         }
         else {
-            $(".file-upload").addClass('active');
-            $("#noFile").text(filename.replace("C:\\fakepath\\", ""));
+            $(".file-upload1").addClass('active');
+            console.log("hi");
+            $("#noFile1").text(filename.replace("C:\\fakepath\\", ""));
+        }
+    });
+    $('#chooseFile2').bind('change', function () {
+        var filename = $("#chooseFile2").val();
+        if (/^\s*$/.test(filename)) {
+            $(".file-upload2").removeClass('active');
+            $("#noFile2").text("No file chosen...");
+        }
+        else {
+            $(".file-upload2").addClass('active');
+            console.log("hi");
+            $("#noFile2").text(filename.replace("C:\\fakepath\\", ""));
+        }
+    });
+    $('#chooseFile3').bind('change', function () {
+        var filename = $("#chooseFile3").val();
+        if (/^\s*$/.test(filename)) {
+            $(".file-upload3").removeClass('active');
+            $("#noFile3").text("No file chosen...");
+        }
+        else {
+            $(".file-upload3").addClass('active');
+            console.log("hi");
+            $("#noFile3").text(filename.replace("C:\\fakepath\\", ""));
+        }
+    });
+    $('#chooseFile4').bind('change', function () {
+        var filename = $("#chooseFile4").val();
+        if (/^\s*$/.test(filename)) {
+            $(".file-upload4").removeClass('active');
+            $("#noFile4").text("No file chosen...");
+        }
+        else {
+            $(".file-upload4").addClass('active');
+            console.log("hi");
+            $("#noFile4").text(filename.replace("C:\\fakepath\\", ""));
         }
     });
 });
