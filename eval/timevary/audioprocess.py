55b654ab2cd2670553319184764f2a33abc026ce
Tina Wu <tinawu.nd23@gmail.com>
2019-12-18 23:59:56 -0500

    final eval

--- eval/timevary/audioprocess.py
+++ eval/timevary/audioprocess.py
@@ -1,19 +1,75 @@
 from pydub import AudioSegment
 
-audio = AudioSegment.from_wav("../static/files/user1.wav")
+audio = AudioSegment.from_wav("../gendervary/tina5s.wav")
 length = audio.duration_seconds
-half = (length//2) * 1000
+half = 2 * 1000
+wav1 = audio[:half]
+wav1.export('./2s/user1-key.wav', format="wav")
+
+
+audio = AudioSegment.from_wav("../gendervary/tina5s--2.wav")
+length = audio.duration_seconds
+half = 2 * 1000
 wav1 = audio[:half]
 wav1.export('./2s/user1.wav', format="wav")
 
-audio = AudioSegment.from_wav("../static/files/user2.wav")
+
+audio = AudioSegment.from_wav("../gendervary/kat5s.wav")
 length = audio.duration_seconds
-half = (length//2) * 1000
+half = 2 * 1000
 wav1 = audio[:half]
 wav1.export('./2s/user2.wav', format="wav")
 
-audio = AudioSegment.from_wav("../static/files/user3.wav")
+audio = AudioSegment.from_wav("../gendervary/luke5s.wav")
 length = audio.duration_seconds
-half = (length//2) * 1000
+half = 2 * 1000
 wav1 = audio[:half]
 wav1.export('./2s/user3.wav', format="wav")
+
+## ==================================================##
+
+audio = AudioSegment.from_wav("../gendervary/tina5s.wav")
+length = audio.duration_seconds
+half = 5 * 1000
+wav1 = audio[:half]
+wav1.export('./5s/user1-key.wav', format="wav")
+
+
+audio = AudioSegment.from_wav("../gendervary/tina5s--2.wav")
+length = audio.duration_seconds
+half = 5* 1000
+wav1 = audio[:half]
+wav1.export('./5s/user1.wav', format="wav")
+
+
+audio = AudioSegment.from_wav("../gendervary/kat5s.wav")
+length = audio.duration_seconds
+half = 5 * 1000
+wav1 = audio[:half]
+wav1.export('./5s/user2.wav', format="wav")
+
+audio = AudioSegment.from_wav("../gendervary/luke5s.wav")
+length = audio.duration_seconds
+half = 5 * 1000
+wav1 = audio[:half]
+wav1.export('./5s/user3.wav', format="wav")
+
+## ==================================================##
+
+audio = AudioSegment.from_wav("../gendervary/tina5s.wav")
+wav1 = audio+audio
+wav1.export('./10s/user1-key.wav', format="wav")
+
+
+audio = AudioSegment.from_wav("../gendervary/tina5s--2.wav")
+wav1 = audio+audio
+wav1.export('./10s/user1.wav', format="wav")
+
+
+audio = AudioSegment.from_wav("../gendervary/kat5s.wav")
+wav1 = audio+audio
+wav1.export('./10s/user2.wav', format="wav")
+
+audio = AudioSegment.from_wav("../gendervary/luke5s.wav")
+wav1 = audio+audio
+wav1.export('./10s/user2.wav', format="wav")
